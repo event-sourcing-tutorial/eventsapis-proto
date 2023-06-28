254e709616d3e3edc49e886fe9c157b3d9d2b115
Abdulaziz Ghuloum <aghuloum@gmail.com>
2023-06-28 16:39:32 +0200

    fixed package name and main file path

--- src-ts/index.ts
+++ src-ts/index.ts
@@ -1,2 +1,10 @@
 
 export * from "./proto/eventsapis";
+
+// import {ChannelCredentials} from "@grpc/grpc-js";
+// import { EventsApisClient, PollEventsResponse } from "./proto/eventsapis";
+//
+// const client = new EventsApisClient("foo.var", ChannelCredentials.createInsecure());
+// const stream = client.pollEvents({lastIdx: BigInt(0)});
+// stream.on("data", (x: PollEventsResponse) => {
+// });
